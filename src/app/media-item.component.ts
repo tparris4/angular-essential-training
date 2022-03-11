04ed1df70c57bdf3d10f2cfdf7ac92f03ae2c327
typar <typarris421@gmail.com>
2022-03-10 19:16:15 -0500

    init

--- src/app/media-item.component.ts
+++ src/app/media-item.component.ts
@@ -1,8 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'mw-media-item',
   templateUrl: './media-item.component.html',
   styleUrls: ['./media-item.component.css']
 })
-export class MediaItemComponent {}
+export class MediaItemComponent {
+  @Input() mediaItem;
+
+  name = 'The Redemption';
+  wasWatched() {
+    return true;
+  }
+  onDelete() {
+    console.log('deleted')
+  }
+}
